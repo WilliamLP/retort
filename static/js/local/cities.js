c6a154733cad6c6d91a5d244d24a9a9961b986e5
William <william@freshbooks.com>
2014-02-21 18:15:03 -0500

    WIP

--- static/js/local/cities.js
+++ static/js/local/cities.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var app = app || {};
 
 app.Constants = {
@@ -16,36 +18,29 @@ app.City = Backbone.Model.extend({
     initialize: function() {
         console.log("City initialize!");
         this.turns = new app.CityTurnList();
-        this.tileGrid = new app.TileGrid();
-    },
-    generateNextTurn: function(curTurn) {
-        var nextTurn = new app.CityTurn();
-        nextTurn.set("turn", curTurn.get('turn') + 1);
-        nextTurn.set("pop", curTurn.get('pop'));
-        nextTurn.set("food", curTurn.get('food'));
-        nextTurn.set("hammers", curTurn.get('hammers'));
+        var prevTurn = null;
+        for(var i=0; i<app.Constants.TURN_COUNT; i++) {
+            var turn = new app.CityTurn();
+            if (prevTurn) {
+                turn.prevTurn = prevTurn;
+            }
+            this.turns.add(turn);
+            prevTurn = turn;
+        }
 
-        return nextTurn;
     },
     generateTurns: function() {
         console.log("Generate turns!");
 
-        this.turns.reset();
-
-        var firstTurn = new app.CityTurn();
+        var firstTurn = this.turns.at(0);
         firstTurn.set("turn", this.get("start_turn"));
         firstTurn.set("pop", this.get("start_pop"));
         firstTurn.set("food", this.get("start_food"));
         firstTurn.set("hammers", this.get("start_hammers"));
 
-
-        this.turns.add(firstTurn);
-
-        var curTurn = firstTurn;
         for(var i=1; i<app.Constants.TURN_COUNT; i++) {
-            nextTurn = this.generateNextTurn(curTurn);
-            this.turns.add(nextTurn);
-            curTurn = nextTurn;
+            var turn = this.turns.at(i);
+            turn.calculateFromPrevious()
         }
 
         this.trigger("reset");
@@ -62,6 +57,18 @@ app.CityTurn = Backbone.Model.extend({
             actions: [],
             results: []
         };
+    },
+    initialize: function() {
+        this.tileGrid = new app.TileGrid();
+    },
+    calculateFromPrevious: function() {
+        var prev = this.prevTurn;
+        this.set("turn", prev.get('turn') + 1);
+        this.set("pop", prev.get('pop'));
+        this.set("food", prev.get('food'));
+        this.set("hammers", prev.get('hammers'));
+
+        this.tileGrid.setTiles(prev.tileGrid);
     }
 });
 
@@ -75,19 +82,40 @@ app.TileGrid = Backbone.Model.extend({
     getTile: function(x, y) {
         return this.tiles[x][y];
     },
+    setTile: function(x, y, attrs) {
+        this.tiles[x][y].f = attrs.f;
+        this.tiles[x][y].h = attrs.h;
+        this.tiles[x][y].c = attrs.c;
+
+        this.trigger('change');
+    },
+    setTiles: function(sourceTileGrid) {
+        for(var x=0; x<5; x++) {
+            for(var y=0;  y<5; y++) {
+                this.setTile(x, y, sourceTileGrid.getTile(x,y));
+            }
+         }
+    },
     initialize: function() {
         this.tiles = [];
         for(var x=0; x<5; x++) {
             var col = []
             for(var y=0;  y<5; y++) {
-                col.push({
+                var tile = {
                     "f": 0,
                     "h": 0,
                     "c": 0,
                     "x": x,
                     "y": y,
                     "valid": !((x==0 || x==4) && (y==0 || y==4)) // Corners are not valid selections
-                })
+                };
+
+                if (x==2 && y==2) {
+                    tile.f = 2;
+                    tile.h = 1;
+                    tile.c = 1;
+                }
+                col.push(tile);
             }
             this.tiles.push(col);
         }
@@ -102,6 +130,7 @@ app.CityTurnView = Backbone.View.extend({
     render: function() {
         console.log("Turn render! %o", this.model.toJSON());
         this.$el.html(this.template(this.model.toJSON()));
+        this.$el.attr('data-cid', this.model.cid);
         return this;
     },
     initialize: function() {
@@ -129,35 +158,56 @@ app.TileGridView = Backbone.View.extend({
         return this;
     },
     initialize: function(opts) {
-        this.tilePopupView = opts.tilePopupView;
-
+        this.tilePopupView = new app.TilePopupView({});
+        if (opts.model) {
+            this.setModel(opts.model);
+        }
+    },
+    setModel: function (model) {
+        this.stopListening(this.model);
+        this.model = model;
+        this.listenTo(this.model, "change", this.render);
         this.render();
     },
-
     editTile: function(e) {
         console.log("Edit tile! %o", this);
         var $el = $(e.currentTarget);
         var x = parseInt($el.attr('data-x'));
         var y = parseInt($el.attr('data-y'));
-        var tile = this.model.getTile(x,y);
-        this.tilePopupView.popup(tile)
+
+        this.tilePopupView.popup(this.model, x, y);
     }
 });
 
 app.TilePopupView = Backbone.View.extend({
     el: $("#tile-popup"),
-    template: _.template($('#tile-popup-template').html()),
     events: {
+        "hide.bs.modal": "closeView"
     },
-    popup: function(tile) {
-        console.log("Tile popup! %o", tile);
+    popup: function(model, x, y) {
+        console.log("Tile popup! %o");
 
-        this.$el.find('.modal-content').html(this.template({}));
-        this.$el.modal();
+        this.model = model;
+        this.tile = model.getTile(x, y);
+
+        this.$el.modal('show');
+        this.$('.btn[name="Food"][value="' + this.tile.f + '"]').button('toggle');
+        this.$('.btn[name="Prod"][value="' + this.tile.h + '"]').button('toggle');
+        this.$('.btn[name="Commerce"][value="' + this.tile.c + '"]').button('toggle');
+
 
         return this;
     },
     initialize: function() {
+    },
+    closeView: function() {
+        console.log('Close view!');
+        var f = parseInt(this.$('.btn.active[name="Food"]').attr('value'));
+        var h = parseInt(this.$('.btn.active[name="Prod"]').attr('value'));
+        var c = parseInt(this.$('.btn.active[name="Commerce"]').attr('value'));
+
+        this.model.setTile(this.tile.x, this.tile.y, {"f": f, "h": h, "c": c});
+        this.model.trigger("change");
     }
 });
 
@@ -167,22 +217,19 @@ app.CityView = Backbone.View.extend({
     headerTemplate: _.template($('#city-header-template').html()),
 
     events: {
-
+        "click.tr": "selectRow"
     },
     initialize: function() {
         this.listenTo(this.model, 'reset', this.addAll);
 
-        // Subviews
-        this.tilePopupView = new app.TilePopupView({});
-        this.tileGridView = new app.TileGridView({tilePopupView: this.tilePopupView, model: app.city.tileGrid});
+        this.tileGridView = new app.TileGridView({});
 
         console.log("Initialize!");
 
         this.render();
 
         this.model.generateTurns();
-        //app.Sequences.fetch();
-        //console.log('fetched %o', app.Sequences);
+        this.selectTurn(this.model.turns.first().cid);
     },
     render: function() {
         console.log('render!');
@@ -192,16 +239,29 @@ app.CityView = Backbone.View.extend({
     addOne: function(item) {
         console.log('add one! %o', item);
         var turnView = new app.CityTurnView({model: item});
-        rendered = turnView.render().el
+        var rendered = turnView.render().el;
         this.$("#city-turns").append(rendered);
     },
     addAll: function() {
         console.log('add all! %o', this.model.turns);
         this.model.turns.each(this.addOne, this);
+    },
+    selectTurn: function(turnCid) {
+        this.selectedTurn = turnCid;
+
+        this.tileGridView.setModel(this.model.turns.get(this.selectedTurn).tileGrid);
+
+        this.$('tr.info').removeClass('info');
+        this.$('tr[data-cid=' + turnCid + ']').addClass('info');
+    },
+    selectRow: function(e) {
+        console.log("Select row!");
+        this.selectTurn($(e.target).parent('tr').attr('data-cid'));
     }
 })
 
 $(function() {
     app.city = new app.City();
     app.cityView = new app.CityView({model: app.city});
+
 });
